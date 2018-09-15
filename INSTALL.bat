2668179933f2bcabe9d76d85b729a10f29cd97f9
sergey <konferovich.sergey@yandex.ru>
2018-09-15 20:24:02 +0500

    'service'

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,6 +1,6 @@
 @echo off
-bitsadmin.exe /transfer "LOL" /priority FOREGROUND https://raw.githubusercontent.com/bugai66/Elysium_Files/master/INSTALL.7z "%~dp0INSTALL.7z"
-bitsadmin.exe /transfer "LOL" /priority FOREGROUND https://raw.githubusercontent.com/bugai66/Elysium_Files/master/7z.exe "%~dp07z.exe"
+bitsadmin.exe /transfer "TRANSFER" /priority FOREGROUND https://raw.githubusercontent.com/bugai66/Elysium_Files/master/INSTALL.7z "%~dp0INSTALL.7z"
+bitsadmin.exe /transfer "TRANSFER" /priority FOREGROUND https://raw.githubusercontent.com/bugai66/Elysium_Files/master/7z.exe "%~dp07z.exe"
 "%~dp0\7z" e "INSTALL.7z"
 start cmd /c "RUN.bat"
 DEL "%~dp07z.exe"
